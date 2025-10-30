13d03a655127dc8f4d0f7af32d5f0ab20c813a78
Sayftey <sayfaziz12345@gmail.com>
2025-10-30 12:21:13 +0000

    Add files via upload

--- Flask practice/App.py
+++ Flask practice/App.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import requests
 
 
-base_url = "https://pokeapi.co/api/v2/pokemon/"
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -19,6 +19,7 @@ def index():
     there = "False"
     ind = "0"
     structured_data = {}
+    base_url = "https://pokeapi.co/api/v2/pokemon/"
     if request.method == "POST":
         pokemon = request.form.get("Content")
         url = base_url + pokemon
@@ -40,17 +41,66 @@ def index():
         else:
             print("error")
             there = "False"
+        
+
     print(ind)
     return render_template('index.html', **structured_data, there=there)
     
 
-    
+
+
+
+@app.route("/search_type", methods=["POST","GET"])
+def search_type():
+    lis = []
+    structured_data_type = {}
+    base_url_type = "https://pokeapi.co/api/v2/type/"
+    base_url_poke = "https://pokeapi.co/api/v2/pokemon/"
+    if request.method == "POST":
+        Type = request.form.get("Content")
+        url_type = base_url_type + Type
+        response_type = requests.get(url_type)
+        if response_type.status_code == 200:
+            structured_data_type = response_type.json()
+            p = 0
+            for i in structured_data_type['pokemon']:
+                print(structured_data_type['pokemon'][p]['pokemon']['name'])
+                url_poke = structured_data_type['pokemon'][p]['pokemon']['url']
+                response_poke = requests.get(url_poke)
+                structured_data_poke = response_poke.json()
+                lis.append(structured_data_poke['sprites']['front_default'])
+                print(lis[p])
+                p +=1 
+
+
+            
+    return render_template("search_type.html", **structured_data_type,lis=lis)
 
 
 
 
 
-
+@app.route("/search_ability", methods=["POST", "GET"])
+def search_ability():
+    lis = []
+    structured_data_type = {}
+    base_url_type = "https://pokeapi.co/api/v2/ability/"
+    if request.method == "POST":
+        Type = request.form.get("Content")
+        url_type = base_url_type + Type
+        response_type = requests.get(url_type)
+        if response_type.status_code == 200:
+            structured_data_type = response_type.json()
+            p = 0
+            for i in structured_data_type['pokemon']:
+                print(structured_data_type['pokemon'][p]['pokemon']['name'])
+                url_poke = structured_data_type['pokemon'][p]['pokemon']['url']
+                response_poke = requests.get(url_poke)
+                structured_data_poke = response_poke.json()
+                lis.append(structured_data_poke['sprites']['front_default'])
+                print(lis[p])
+                p +=1 
+    return render_template("search_ability.html",**structured_data_type, lis=lis)
 
 
 if __name__ == "__main__":
